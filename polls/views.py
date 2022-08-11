e8e2b1d735d2bdc21c365ac1a40ab8ea67f9f358
mojtaba rakhisi <mojtaba.rakhisi@gamil.com>
2022-08-11 19:22:27 +0430

    init

---
+++ polls/views.py
@@ -0,0 +1,7 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello this is my first django app with a good tutorial :)")
