cb203e1a4195905543afa4d253d6e6b0994401f4
mojtaba rakhisi <mojtaba.rakhisi@gamil.com>
2022-08-11 23:39:58 +0430

    feat : add pollsviews and funcs

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,22 @@
-from http.client import HTTPResponse
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from polls.models import Question
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
-    return HttpResponse("Hello this is my first django app with a good tutorial :)")
+    latest_qustion_list = Question.objects.order_by('-pub_date')
+    context = {'latest_question_list': latest_qustion_list}
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question' : question})
+
+
+def results(request, question_id):
+    return HttpResponse("you are looking at results of question %s" % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("you are votion to question : %s" % question_id)
